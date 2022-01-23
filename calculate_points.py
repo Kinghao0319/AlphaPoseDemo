0bcad999d7b81667673c0344a5adf05536bca69a
Kinghao0319 <1018510176@stu.ecnu.edu.cn>
2022-01-24 00:07:57 +0800

    update calc

--- calculate_points.py
+++ calculate_points.py
@@ -5,6 +5,7 @@ points_file=open('./examples/res/point-results.json')
 frame1=5
 frame2=150
 
+# 计算运动前后变化的角度
 def cacl_arm_rotation(frame1,frame2):
     content_list = json.loads(points_file.read())
     f1,f2=content_list[frame1//5-1]['points'],content_list[frame2//5-1]['points']
@@ -42,6 +43,42 @@ def cacl_arm_rotation(frame1,frame2):
     print("大臂抬动角度：" + str(right_a))
     print("输入Rotation值：" + str(origin_a + right_a))
 
+# 计算大臂骨骼的绝对角度
+def calc_arm_degree(frame1,frame2):
+    content_list = json.loads(points_file.read())
+    f1, f2 = content_list[frame1 // 5 - 1]['points'], content_list[frame2 // 5 - 1]['points']
+    left_f1_x1, left_f1_y1 = f1[8][0], f1[8][1]
+    left_f1_x2, left_f1_y2 = f1[6][0], f1[6][1]
+    cal_up=left_f1_y1-left_f1_y2
+    # print(cal_up)
+    cal_down=left_f1_x1-left_f1_x2
+    # print(cal_down)
+    tan_a=cal_up/cal_down
+    # 从图像坐标系转unity坐标系，绝对值-180°
+    left_a=abs(math.atan(tan_a)*180/math.pi)-180
+    print("frame1左arm的角度为："+str(left_a))
+    left_f2_x1, left_f2_y1 = f2[8][0], f2[8][1]
+    left_f2_x2, left_f2_y2 = f2[6][0], f2[6][1]
+    cal_up = left_f2_y1 - left_f2_y2
+    cal_down = left_f2_x1 - left_f2_x2
+    tan_a = cal_up / cal_down
+    left_a = abs(math.atan(tan_a) * 180 / math.pi) - 180
+    print("frame2左arm的角度为：" + str(left_a))
+    right_f1_x1, right_f1_y1 = f1[7][0], f1[7][1]
+    right_f1_x2, right_f1_y2 = f1[5][0], f1[5][1]
+    cal_up = right_f1_y1 - right_f1_y2
+    cal_down = right_f1_x1 - right_f1_x2
+    tan_a = cal_up / cal_down
+    right_a = -math.atan(tan_a) * 180 / math.pi
+    print("frame1右arm的角度为：" + str(right_a))
+    right_f2_x1, right_f2_y1 = f2[7][0], f2[7][1]
+    right_f2_x2, right_f2_y2 = f2[5][0], f2[5][1]
+    cal_up = right_f2_y1 - right_f2_y2
+    cal_down = right_f2_x1 - right_f2_x2
+    tan_a = cal_up / cal_down
+    right_a = -math.atan(tan_a) * 180 / math.pi
+    print("frame2右arm的角度为：" + str(right_a))
+
 #在unity中素材的左右肩坐标
 left_shoulder=[-0.48,5.54]
 right_shoulder=[0.56,5.32]
@@ -63,4 +100,5 @@ def calc_transform(i,frame1,frame2):
 
 if __name__ == '__main__':
     # cacl_arm_rotation(frame1,frame2)
-    calc_transform(6,frame1,frame2)
+    # calc_transform(6,frame1,frame2)
+    calc_arm_degree(frame1,frame2)
